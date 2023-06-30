37bf5efc47718e8986a7a86c60b16e458a6bcf3a
Anderson Lima <111541769+AndersonlimaDev@users.noreply.github.com>
2023-06-30 12:24:13 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ====== Animação do Typing ====== */
+var typed = new Typed(".typing",{
+    strings:["","Designer Gráfico","Desenvolvedor Front-end","Web Designer" ,"UX/UI Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+/* ====== Animação do Typing ====== */
+
