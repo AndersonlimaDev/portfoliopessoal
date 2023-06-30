4f3dbed394a13311346e42192cf2af4c418469fb
Anderson Lima <111541769+AndersonlimaDev@users.noreply.github.com>
2023-06-30 17:56:32 -0300

    Atualizar o script.js
    Ajuste : Desenvolvedor ~> Programador

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ====== Animação do Typing ====== */
 var typed = new Typed(".typing",{
-    strings:["","Designer Gráfico","Desenvolvedor","Web Designer" ,"UX/UI Designer"],
+    strings:["","Designer Gráfico","Programador","Web Designer" ,"UX/UI Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
